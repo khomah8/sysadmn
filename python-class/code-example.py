5fd38293a68a1d46d28917b74cedf2a5466ca2ed
khomah8 <59750654+khomah8@users.noreply.github.com>
2021-01-04 15:47:51 +0200

    Update code-example.py

--- python-class/code-example.py
+++ python-class/code-example.py
@@ -1,6 +1,13 @@
 # from  https://www.w3schools.com/python/ 
+
 print("Hello")
 print('Hello')
 
 a = "Hello"
 print(a)
+
+abb = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(abb)

e12ea7736d6ed89f03603d0df910d894964b9d75
khomah8 <59750654+khomah8@users.noreply.github.com>
2021-01-04 16:49:31 +0200

    Update strings-examples.py

--- python-class/strings-examples.py
+++ python-class/strings-examples.py
@@ -13,5 +13,9 @@ ut labore et dolore magna aliqua."""
 print(abb)
 
 # try on  https://www.w3schools.com/python/trypython.asp?filename=demo_string_slice_start
+
 print(abb[:4])
 print(abb[5:])
+
+bb = "H e l l o,d e a r W o r l d !"
+print(bb[::2])

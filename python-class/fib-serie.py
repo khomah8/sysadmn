37d77137a5156b2083548353a390296909b9c442
khomah8 <59750654+khomah8@users.noreply.github.com>
2021-01-04 13:23:06 +0200

    Update fib-serie.py

--- python-class/fib-serie.py
+++ python-class/fib-serie.py
@@ -4,3 +4,16 @@ a, b = 0, 1
 while a < 10:
     print(a)
     a, b = b, a+b
+
+# output by Python Shell as Chrome extension 
+# >>> while a < 10:
+#    print(a)
+#    a, b = b, a+b
+# ... 
+# 0
+# 1
+# 1
+# 2
+# 3
+# 5
+# 8 

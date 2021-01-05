9a40951e9ba71b937ef93d4dc7865f0b39be0b2f
khomah8 <59750654+khomah8@users.noreply.github.com>
2021-01-05 15:29:46 +0200

    Update lists-data.py

--- python-class/lists-data.py
+++ python-class/lists-data.py
@@ -7,3 +7,7 @@ thislist = ["apple", "banana", "cherry"]
 print(thislist)
 print(len(thislist))
 
+# It is also possible to use the list() constructor when creating a new list.
+
+thislist = list(("apple", "banana", "cherry"))  # note the double round-brackets
+print(thislist)

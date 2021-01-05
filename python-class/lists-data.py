adef6d912b7f2705ca8500e68cb890040698f2f9
khomah8 <59750654+khomah8@users.noreply.github.com>
2021-01-05 15:26:33 +0200

    Update lists-data.py

--- python-class/lists-data.py
+++ python-class/lists-data.py
@@ -1,4 +1,9 @@
 # as https://www.w3schools.com/python/gloss_python_lists.asp
 
+# List items are ordered, changeable, and allow duplicate values.
+# List items are indexed, the first item has index [0], the second item has index [1] etc.
+
 thislist = ["apple", "banana", "cherry"]
 print(thislist)
+print(len(thislist))
+

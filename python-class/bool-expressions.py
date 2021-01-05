41c61a69e25ca932888a508e261d992c104b6774
khomah8 <59750654+khomah8@users.noreply.github.com>
2021-01-05 16:49:58 +0200

    Create bool-expressions.py

---
+++ python-class/bool-expressions.py
@@ -0,0 +1,12 @@
+# https://www.w3schools.com/python/python_booleans.asp
+
+'''
+Almost any value is evaluated to True if it has some sort of content.
+- Any string is True, except empty strings.
+- Any number is True, except 0.
+- Any list, tuple, set, and dictionary are True, except empty ones.
+'''
+
+bool("abc")
+bool(123)
+bool(["apple", "cherry", "banana"])

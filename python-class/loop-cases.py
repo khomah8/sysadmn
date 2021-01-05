a3ea798c6ba094db89fb19f71fe5978e814e1979
khomah8 <59750654+khomah8@users.noreply.github.com>
2021-01-05 14:41:12 +0200

    Update and rename loop-cases.py to loops-for-while.py

--- python-class/loop-cases.py
+++
@@ -1,7 +0,0 @@
-# as https://www.w3schools.com/python/python_for_loops.asp
-fruits = ["apple", "banana", "cherry"]
-for x in fruits:
-  if x == "banana":
-    break
-  print(x)
-  
---
+++ python-class/loops-for-while.py
@@ -0,0 +1,25 @@
+# as https://www.w3schools.com/python/python_for_loops.asp
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    break
+  print(x)
+  
+# https://www.w3schools.com/python/python_while_loops.asp
+
+i = 1
+while i < 6:
+  print(i)
+  if i == 3:
+    break
+  i += 1
+# out of loop 
+  
+i = 0
+while i < 6:
+  i += 1
+  if i == 3:
+    continue
+  print(i)
+# skip some loop 

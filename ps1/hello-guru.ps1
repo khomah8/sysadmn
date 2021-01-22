0f2b4c3de8cf3459c917af30d85310f68a34a394
khomah8 <59750654+khomah8@users.noreply.github.com>
2021-01-22 16:32:27 +0200

    Create hello-guru.ps1

---
+++ ps1/hello-guru.ps1
@@ -0,0 +1,3 @@
+Write-Host "Hello, Guru99!"
+# simple output
+# PowerShell 5.0 was released as internal part of Windows management framework 5. The feature offers in this version are remote debugging, class definitions, .NET enumerations, , etc.

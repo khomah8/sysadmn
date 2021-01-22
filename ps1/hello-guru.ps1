7e4e88f47e61598ddae0ebc4b95a7d8c8c4a74c1
khomah8 <59750654+khomah8@users.noreply.github.com>
2021-01-22 17:46:11 +0200

    Update hello-guru.ps1

--- ps1/hello-guru.ps1
+++ ps1/hello-guru.ps1
@@ -1,6 +1,7 @@
 Write-Host "Hello, Guru99!"
 # simple output
-# PowerShell 5.0 was released as internal part of Windows management framework 5. The feature offers in this version are remote debugging, class definitions, .NET enumerations, , etc.
+# PowerShell 5.0 was released as internal part of Windows management framework 5. 
+# The feature offers in this version are remote debugging, class definitions, .NET enumerations, , etc.
 <#
  multi-line comments are available in PS 2.0 and later 
 #>

e4a308b8fcfaabf5164e7d753c14b7e9eeaa921a
khomah8 <59750654+khomah8@users.noreply.github.com>
2021-01-22 17:48:37 +0200

    Update hello-guru.ps1

--- ps1/hello-guru.ps1
+++ ps1/hello-guru.ps1
@@ -1,7 +1,10 @@
 Write-Host "Hello, Guru99!"
 # simple output
+
+# PowerShell first version 1.0 was released in 2006. Today, PowerShell is at version 5.1. 
 # PowerShell 5.0 was released as internal part of Windows management framework 5. 
 # The feature offers in this version are remote debugging, class definitions, .NET enumerations, , etc.
+
 <#
  multi-line comments are available in PS 2.0 and later 
 #>
